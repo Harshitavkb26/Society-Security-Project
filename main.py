8f40624d05fc0063ccc3d54e85cd66101c96bc4c
harshita <harshitakb123@gmail.com>
2022-05-19 17:03:41 +0530

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import importlib as il
 import mysql.connector
 
 def connectSQLDatabase():
-    server = 'societymembers.database.windows.net'
+    server = 'harshita.database.windows.net'
     database = 'society'
     username = 'Harshi'
     password = 'Anjali123'
@@ -32,3 +32,81 @@ class FaceID(object):
     }
 
     def fetchSQLData(self):
+        #mydb = mysql.connector.connect(server='harshita.database.windows.net',username= 'Harshi',password= 'Anjali123',database='society')
+
+        #mycursor = mydb.cursor()
+
+    def createGroup(self, wing, name):
+
+        params = urllib.parse.urlencode({})
+
+        body = {
+            "name" :'{}'.format(name),
+        }
+
+        try:
+            self.conn.request("PUT","/face/v1.0/persongroups/"+ wing +"?%s" % params, json.dumps(body), self.headers)
+            response = self.conn.getresponse()
+            data = response.read()
+            print("Group Created")
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        
+    def addperson(self, name, targetGroup):
+
+        params = urllib.parse.urlencode({})
+
+        body = {
+            "name": '{}'.format(name),
+        }
+
+        try:
+            self.conn.request("POST","/face/v1.0" + targetGroup + "/persons?%s" % params, json.dumps(body), self.headers)
+            response = self.conn.getresponse()
+            data = response.read()
+            print("Person Added: ", name)
+        
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno,e.strerror))
+    def addFace(self, targetName, targetGroup, URL):
+
+
+    #returns a json list of people in the group
+    def listPersonsInGroup(self, targetGroup):
+
+    #to train the group
+    def trainGroup(self, targetGroup):
+
+    # Returns faceId to be fed into identifyFace, returns -1(integer) if no face found
+    def detectFace(self, imgData):
+
+    # To identify face returns name of the person 
+    def identifyFace(self, faceId, targetGroup):  
+
+    def addPersonToDatabase(self, name, flat, wing)
+
+    # to take entries
+    def takeEntries():
+
+    def getLastPersonScanned(self):
+        return self.personScanned 
+
+    def getPersonDetails(self, name):
+
+    def wipeEntryLog(self, timetablekey):
+
+    def TrainInit(self):
+
+    def DatabaseInit(self):
+
+    def getPersonJson(self):
+
+    def main(self, flag, sub):
+
+
+if __name__ == "__main__":
+    app = FaceID()
+    #flag=1
+
+    app.main(flag,sub)  
+    #app.main() 
